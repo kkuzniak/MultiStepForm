ddde12b3ff1191c114bf580ce00eac62e9d8ebdf
Kacper Kuźniak <kkuzniakgithub@gmail.com>
2026-05-28 14:53:24 +0200

    chore: add vitest configuration with coverage

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
+    coverage: {
+      provider: 'v8',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+});
